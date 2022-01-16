165c7de0997598a77d04fcc1494ff3ae819a74c0
Satyam Gothi <41023447+RogueSMG@users.noreply.github.com>
2022-01-17 01:03:38 +0530

    Update inscope.sh

--- inscope.sh
+++ inscope.sh
@@ -1,10 +1,14 @@
-if [[ -z $1 ]] || [[ -z $2 ]]
+#!/bin/bash
+
+nmap -sL -n -iL ips.txt | awk '/Nmap scan report/{print $NF}' >> all_ips_scope.txt
+
+if [[ -z $1 ]]
 then
-	echo "Usage: inscope.sh [DOMAIN_NAMES] [IN_SCOPE_IPS]"
+	echo "Usage: inscope.sh [DOMAIN_NAMES]"
 	exit
 fi
 DOMAINS=$(cat $1)
-SCOPE=$2
+SCOPE=all_ips_scope.txt
 for i in $DOMAINS
 do
 	IP=$(dig +short $i)
